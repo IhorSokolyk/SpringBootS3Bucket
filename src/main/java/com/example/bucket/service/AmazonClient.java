182d17651c3bcca96cfd5b22f4a029edad03262c
NischayaSharma <nischaya1703@gmail.com>
2020-09-10 19:51:24 +0530

    fix: Updated the code for working with JAVA 12

--- src/main/java/com/example/bucket/service/AmazonClient.java
+++ src/main/java/com/example/bucket/service/AmazonClient.java
@@ -2,11 +2,13 @@ package com.example.bucket.service;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -34,7 +36,10 @@ public class AmazonClient {
     @PostConstruct
     private void initializeAmazon() {
         AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-        this.s3client = new AmazonS3Client(credentials);
+        this.s3client = AmazonS3Client.builder()
+        .withRegion("us-east-2")
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .build();
     }
 
     public String uploadFile(MultipartFile multipartFile) {
@@ -64,8 +69,12 @@ public class AmazonClient {
     }
 
     private void uploadFileTos3bucket(String fileName, File file) {
-        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
-                .withCannedAcl(CannedAccessControlList.PublicRead));
+        PutObjectRequest request = new PutObjectRequest(bucketName, fileName, file);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType("plain/text");
+        metadata.addUserMetadata("title", "someTitle");
+        request.setMetadata(metadata);
+        s3client.putObject(request);
     }
 
     public String deleteFileFromS3Bucket(String fileUrl) {
